fea953a0378a899d44591edaf39ee28bf92b7c70
Michael Johnson <michaelbojohnson@gmail.com>
2022-03-16 18:33:11 -0500

    commit tree 730f979f53dccca958eeafe9d9cf875589e5e72c

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//1.get access to each sentence
+//2. touch each word
+//3. capitalize each first letter only
+//4. return the array
+
+
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((phrase) => {
+    const words = phrase.split(" ");
+    const catPhrase = words.map((word) => {
+     return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    const answer = catPhrase.join(" ")
+    return answer;
+  });
+
+  
+
+
+   
+
+/*const titleCased = () => {
+  let words = tutorials.split(" ");
+  let firstLetter = tutorials.map(element => words.charAt(0).toUpperCase());
+  let restOf = tutorials.map(element => element.slice(1));
+  console.log(words);
+  return firstLetter + restOf;*/
 }
+
